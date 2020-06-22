b0467995f9d31389bc453ab6961878228deb8933
pedrobn23 <pedrobonillanadal@hotmail.com>
2020-06-22 20:29:29 +0200

    bug Hamilton Solucionado.

--- src/hamilton.py
+++ src/hamilton.py
@@ -1,5 +1,6 @@
 from graph import Graph
 from pysat.solvers import Solver
+from pysat.card import CardEnc, EncType
 
 
 def find_hamiltonian_path(graph):
@@ -96,67 +97,64 @@ def find_hamiltonian_path2(graph):
         names[integer + 1] = vertex
     names[0] = names[length]
 
-    cnf = CardEnc.equals(lits=[1, 2, 3], encoding=EncType.pairwise)
-    print(cnf)
     # Every position in the path must be occupied
-    for position_in_path in range(1, length + 2):
-        solver.add_clause([
+    for position_in_path in range(length):
+        var_list = [
             position_in_path * length + vertex
             for vertex in range(1, length + 1)
-        ])
+        ] 
+
+        cnf = CardEnc.equals(lits=var_list, encoding=EncType.pairwise)
+        print(cnf.clauses)
+        solver.append_formula(cnf)
+    print()
 
     # Every vertex must have a position in path
     for vertex in range(1, length + 1):
-        solver.add_clause([
+        var_list = [
             position_in_path * length + vertex
-            for position_in_path in range(1, length + 2)
-        ])
+            for position_in_path in range(length)
+        ]
+        
+        cnf = CardEnc.equals(lits=var_list, encoding=EncType.pairwise)
+        solver.append_formula(cnf)
+        print(cnf.clauses)
 
-    # Only one vertex must have a position in path
-    for position_in_path in range(1, length + 2):
-        for vertex_a in range(1, length + 1):
-            for vertex_b in range(vertex_a + 1, length + 1):
-                solver.add_clause([
-                    -(position_in_path * length + vertex_a),
-                    -(position_in_path * length + vertex_b)
-                ])
-
-    #every vertes must have only one position
-    for position_in_path in range(1, length + 1):
-        for position_in_path2 in range(position_in_path + 1, length + 1):
-            for vertex_a in range(1, length + 1):
-                solver.add_clause([
-                    -(position_in_path * length + vertex_a),
-                    -(position_in_path2 * length + vertex_a)
-                ])
-
-        if position_in_path > 1:
-            for vertex_a in range(1, length + 1):
-                solver.add_clause([
-                    -(position_in_path * length + vertex_a),
-                    -((length + 1) * length + vertex_a)
-                ])
-
+    print()
     # Every two consecutive vertex has to be adjacent
     edges = graph.edges()
     for vertex_a in range(1, length + 1):
-        for vertex_b in range(vertex_a, length + 1):
+        for vertex_b in range(vertex_a+1, length + 1):
+            print(vertex_a, vertex_b)
             if (names[vertex_a], names[vertex_b]) not in edges:
-                for position_in_path in range(1, length + 1):
+                print('not consecutive')
+                for position_in_path in range(length-1):
                     solver.add_clause([
                         -(position_in_path * length + vertex_a),
                         -((position_in_path + 1) * length + vertex_b)
                     ])
+                    solver.add_clause([
+                        -(position_in_path * length + vertex_b),
+                        -((position_in_path + 1) * length + vertex_a)
+                    ])
+
+                solver.add_clause([-vertex_b,-(length*(length-1) + vertex_a)  ])
+                solver.add_clause([-vertex_a,-(length*(length-1) + vertex_b)  ])
+
+
 
     solution = []
-    solver.solve()
     if solver.solve():
         for variable in solver.get_model():
-            if variable > 0 and variable > length:
+            if variable > 0:
                 solution.append(names[variable % length])
 
     return solution
 
-graph = Graph()
-graph.add_from_text('graphs/structured-type1-100nodes.txt')
+
+
+
+g2 = {"a": {"b","c"}, "b": {"c","a"}, "c": {"b","a"}}
+graph = Graph(g2)
+# graph.add_from_text('graphs/structured-type1-100nodes.txt')
 print(find_hamiltonian_path2(graph))
