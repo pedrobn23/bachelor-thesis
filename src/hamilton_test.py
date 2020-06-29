6c4d3df63b91aa9348cc40c58cdf7252f33b533d
pedrobn23 <pedrobonillanadal@hotmail.com>
2020-06-29 10:00:46 +0200

    fallos de la introducción corregidos

--- src/hamilton_test.py
+++ src/hamilton_test.py
@@ -1,6 +1,7 @@
 import random
 import time
 from graph import Graph
+import networkx as nx
 
 
 def random_graph(n_vertices, n_edges):
@@ -15,6 +16,28 @@ def random_graph(n_vertices, n_edges):
 
     return graph
 
+def internet_hamilton(edges):
+    G = nx.Graph()
+    G.add_edges_from(edges)
+
+    F = [(G,[list(G.nodes())[0]])]
+    n = G.number_of_nodes()
+    while F:
+        print(len(F))
+        graph,path = F.pop()
+        confs = []
+        for node in graph.neighbors(path[-1]):
+            conf_p = path[:]
+            conf_p.append(node)
+            conf_g = nx.Graph(graph)
+            conf_g.remove_node(path[-1])
+            confs.append((conf_g,conf_p))
+        for g,p in confs:
+            if len(p)==n:
+                return p
+            else:
+                F.append((g,p))
+    return None
 
 def experiment(graph):
     print('Graph info:')
@@ -30,18 +53,20 @@ def experiment(graph):
     print(path)
     print('\n\n')
 
-    # print('Backtrack solving - ')
-    # start_time = time.time()
-    # path = hamilton.backtrack_hamilton(graph, graph.vertices()[0])
-    # print('Solving last: ', (time.time() - start_time), 'segs.')
+    print('Backtrack solving - ')
+    start_time = time.time()
+    path =     internet_hamilton(list(graph.edges()))
+    print('Solving last: ', (time.time() - start_time), 'segs.')
     # print('Checking correctness:', hamilton.check_correctness(graph, path))
-    # print('\n\n')
+    print('\n\n')
 
 
 if __name__ == '__main__':
     graph_list = [
         'graphs/structured-type1-100nodes.txt',
-        'graphs/structured-type1-400nodes.txt'
+        'graphs/structured-type1-400nodes.txt',
+        'graphs/structured-type1-900nodes.txt',
+        'graphs/structured-type1-1600nodes.txt'
     ]
 
     for filename in graph_list:
