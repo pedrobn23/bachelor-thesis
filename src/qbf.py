f04c44633dfe8215eb16427bc84aa6ad5faf31e5
pedrobn23 <pedrobonillanadal@hotmail.com>
2020-06-29 13:50:27 +0200

    casi entregamos

--- src/qbf.py
+++ src/qbf.py
@@ -10,38 +10,66 @@ from pysat.formula import IDPool, CNF
 
 
 class NaiveQBF():
-
     def __init__(self):
-        self.formula = CNF
+        """
+        Constructor of the class. Always create an empty solver
+        """
+        self.formula = CNF()
         self.quantifiers = []
         self.propagate = []
 
     def solve(self):
+        """
+        Solved the associated formula.
+        """
         return __solve(self.quantifiers, self.formula, self.propagate)
 
     def append_formula(self, formula):
+        """
+        Append a formula (seen as a list of clauses) to the solver.
+        """
         for clause in append_formula:
             self.formula.add_clause(clause)
 
     def propagate(self, variable):
+        """
+        Add a literal to be propagated.
+        """
         self.propagate.append(variable)
         if variable in self.quantifiers:
             self.quantifiers.remove(variable)
 
     def add_clause(self, clause):
+        """
+        Add a clause to the formula
+        """
+        
         self.formula.add_clause(clause)
 
     def add_quantifiers(self, quantifier):
+        """
+        Add a quantifier to the quantifiers list. 
+        Quantifiers are represented as integers. A positive
+        quantifiers is interpreted as an exists, as well as
+        a negative quantifiers is interpreted as a for all.
+        """
+
         if quantifier in self.quantifiers:
             raise ValueError('A variable can not be quantified twice')
 
         self.quantifiers.append(quantifier)
 
     def negate(self):
+        """
+        negate the formula along with the quantifiers list. 
+        """
         self.formula = self.formula.negate()
         self.quantifiers = [-quant for quant in self.quantifiers]
 
     def __solve(quantifiers, formula, propagate):
+        """
+        Private method that implements the recursión that solve the problem.
+        """
         if quantifiers:
             new_quant = copy.deepcopy(quantifiers)
             quantifier = new_quant.pop()
